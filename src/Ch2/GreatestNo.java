09e7c34209a990744b50efb0b980de5374612590
PAN <naresuha@gmail.com>
2018-03-11 18:37:28 -0400

    Program using Operator & control Statement.

---
+++ src/Ch2/GreatestNo.java
@@ -0,0 +1,29 @@
+// Program to calculate Largest No Among available Nos.
+/**
+ * @author Naresh
+ * @Year 2018
+ * @Version 1.1
+ */
+
+package Ch2;
+
+public class GreatestNo {
+
+	public static void main(String[] args) {
+		int num1,num2,num3;
+		num1 = 25;
+		num2 = 100;
+		num3 = 89;
+		
+		if (num1 > num2 & num1 > num3)
+		{
+			System.out.println("Largest No is:" + num1);
+		}
+			else 
+				if (num2 > num1 & num2 > num3)
+				System.out.println("Largest No is: " + num2);
+		
+			else
+			System.out.println("Largest No is: " + num3);
+		}
+	}
