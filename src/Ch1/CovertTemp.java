0298bd53c3bdbce2589e1e2e02b1e4463f4d184e
PAN <naresuha@gmail.com>
2018-03-10 22:13:50 -0500

    Ch1 Program on Eclipse & attempt to export to git repository.

---
+++ src/Ch1/CovertTemp.java
@@ -0,0 +1,25 @@
+//Demonstrate converting Temperature
+
+/**
+*@Auther Naresh
+*@since 2018
+*@Version 1.0
+*/
+
+package Ch1;
+
+public class CovertTemp {
+
+	public static void main(String[] args) {
+
+		double C,F;
+		
+		F = 212.0;
+		
+		C = (F - 32) / 1.8;
+		
+		System.out.println("Temp is Celcicus: " + C);
+
+	}
+
+}
