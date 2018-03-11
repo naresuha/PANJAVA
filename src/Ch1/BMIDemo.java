78acd8fada1d09173e4931fbc7b134ee131a19b8
PAN <naresuha@gmail.com>
2018-03-11 15:46:40 -0400

    Program using Data Type & casting

---
+++ src/Ch1/BMIDemo.java
@@ -0,0 +1,28 @@
+//Program to Compute Body Mass Index
+
+/**
+ * @Author Naresh
+ * @Year 2018
+ * @Version 1.1
+ */
+
+package Ch1;
+
+public class BMIDemo {
+
+	public static void main(String[] args) {
+
+		float P,In,BMI,M;
+		P = 452F;
+		In = 72F;
+		
+		P = (float)(P * 0.4536);
+		In = (float) (In * 0.0254);
+		M = In * In;
+		
+		BMI = (P / M);
+		
+		System.out.println("BMI is: " + BMI);
+	}
+
+}
