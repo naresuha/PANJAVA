5abcb841edcebc73c8ef7fd665dcc7202331fe99
PAN <naresuha@gmail.com>
2018-03-13 20:29:17 -0400

    Sample Program using If-else Control statement

---
+++ src/Ch2/CheckResult.java
@@ -0,0 +1,26 @@
+//Program to Check Exam Result, If Marks above 50 pass else fail.
+/**
+ * @Author Naresh P
+ * @Yr 2018
+ * @Version 1.1
+ */
+
+package Ch2;
+
+public class CheckResult {
+
+	public static void main(String[] args) {
+
+		int marks;
+		
+		marks = 51;
+		
+		if (marks >= 50)
+		{
+			System.out.println ("Student passed in exam"); 
+		}
+			else
+			System.out.println("Student failed in Exam");
+	}
+
+}
