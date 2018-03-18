7bd7f8084797dce45c3e661cb1f061d9bc692916
PAN <naresuha@gmail.com>
2018-03-18 07:28:16 -0400

    Program with Constructor & Method overload

---
+++ src/Ch7/AreaOfCircleNCube.java
@@ -0,0 +1,42 @@
+//Define two areas to accomodate constructor & method overloading 
+/**
+ * author Naresh P
+ * since 2018
+ * version 1.1
+ */
+
+package Ch7;
+
+class areas{
+	
+	float r;
+	double a;
+	float pi;
+	
+	areas(float r,float pi){ //overloading constructor 'areas'
+		this.r = r;
+		this.pi = pi;
+	}
+	areas(double edge){
+		edge = a;
+	}
+	
+	double vol(double a){ // overloading method 'vol'
+		return (6 * a * a);
+	}
+	float vol(){
+		return (pi * r * r );
+	}
+}
+
+public class AreaOfCircleNCube {
+	public static void main(String agrs[]){
+		
+		areas C1 = new areas(2.0f,3.14f);
+		areas C2 = new areas(3.0d);
+		
+		System.out.println("Area of Circle: "+ C1.vol());
+		System.out.println("Area of Cube: " + C2.vol(3.0d));
+	}
+	
+}
